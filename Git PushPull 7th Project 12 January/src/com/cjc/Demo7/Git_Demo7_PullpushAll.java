1b489f0a48a1f3914973273429d6f7456bf60c35
rutur <rutur@192.168.1.71>
2024-01-12 10:16:24 +0530

    By Ruturaj

---
+++ Git PushPull 7th Project 12 January/src/com/cjc/Demo7/Git_Demo7_PullpushAll.java
@@ -0,0 +1,25 @@
+package com.cjc.Demo7;
+
+public class Git_Demo7_PullpushAll 
+{
+	public void m7() 
+	{
+		System.out.println("This is Method m7");
+	}
+	
+	public void m77() 
+	{
+		System.out.println("This is Method m77");
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("main Method m7");	
+		Git_Demo7_PullpushAll  d7 = new Git_Demo7_PullpushAll ();
+		d7.m7();
+		d7.m77();
+	
+	}
+
+}
